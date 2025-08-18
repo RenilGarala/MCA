1c2c0e8a80bfe9f6495cda870fe994cb17810d00
renil <renilgarala360004@gmail.com>
2025-08-18 12:17:07 +0530

    Extra Program

---
+++ Lab5/extra.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+int main(){
+    int number;
+
+    printf("Enter 4 digit number = ");
+    scanf("%d", &number);
+
+    int n = 1000;
+
+    while(number != 0){
+        int remaing = number / n;
+
+        int last_char = remaing%10;
+
+        if(last_char == 1){
+            printf("One ");
+        } else if(last_char == 2){
+            printf("Two ");
+        } else if(last_char == 3){
+            printf("Three ");
+        } else if(last_char == 4){
+            printf("Four ");
+        } else if(last_char == 5){
+            printf("Five ");
+        } else if(last_char == 6){
+            printf("Six ");
+        } else if(last_char == 7){
+            printf("Seven ");
+        } else if(last_char == 8){
+            printf("Eight ");
+        } else if(last_char == 9){
+            printf("Nine ");
+        } else if(last_char == 0){
+            printf("Zero ");
+        }
+
+        remaing = remaing / 10;
+        n = n / 10;
+        if(n==0){
+            break;
+        }
+    }
+    return 0;
+}
+
+
+
+// 3 4 5 6
+// 3 4 5
+// 3 4 
+// 3
+
+// 3
+// 3 4 
+// 3 4 5 
